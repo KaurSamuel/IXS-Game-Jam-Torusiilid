71ac382b0103d531f91967ac70960d1a6a501912
Kaur Samuel <Kaursamuel.kannel@gmail.com>
2022-05-22 14:12:33 +0300

    yoyoyoyoyooyooyoyoyoy

--- Assets/Scripts/ScoreHolder.cs
+++ Assets/Scripts/ScoreHolder.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 public static class ScoreHolder
 {
     public static int Score { get; set; } = 0;
+    public static int EnemiesKilled { get; set; } = 0;
     public static float Volume { get; set; } = 0.5f;
 }        
